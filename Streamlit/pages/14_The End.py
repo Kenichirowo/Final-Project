5db00b88f5bd1dbb0e8bd2a198791ba223a297bb
bvictora <vrr46@hotmail.com>
2025-03-12 12:48:50 +0100

    Update 14_The End.py

--- Streamlit/pages/14_The End.py
+++ Streamlit/pages/14_The End.py
@@ -3,25 +3,25 @@ import streamlit as st
 # Set page configuration to wide mode for a larger container
 st.set_page_config(layout="wide")
 
-# Starting the app
+# The end image
 st.image("img/theend.jpg", width=1000)
 
 st.write("")
 st.write("")
 
-
 # Create columns for layout
 col1, col2, col3 = st.columns([1, 2, 1])  # Adjust the ratio to fit your design
 
-# Display the header in the first column
+# first column
 with col1:
     st.image("img/ironhack.png", width=100)
 
-# Display the button and balloons in the second column
+# second column
 with col2:
     st.header("By Víctor Ramírez Rubio")
 
-# Display the image in the third column
+# third column
 with col3:
     if st.button("Thank you!"):
+        st.audio("sound/indy_motif.mp3", , autoplay=True) # Trigger the audio
         st.balloons()
